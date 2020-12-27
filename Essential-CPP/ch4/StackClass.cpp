71aa5d42a81267590858407fb0358660d2f03cb5
sgfcz <973829105@qq.com>
2020-12-28 01:36:51 +0800

    update

--- Essential-CPP/ch4/StackClass.cpp
+++ Essential-CPP/ch4/StackClass.cpp
@@ -1,9 +1,20 @@
 #include "StackClass.h"
 
+using namespace std;
+
 void fill_stack(Stack &stack, istream &is = cin)
 {
     string str;
-    while (is >> str && !stack.full())
+    while (is >> str && !stack.full()) //ctrl+d 为结束
         stack.push(str);
+
     cout << "Read in " << stack.size() << " elements\n";
+    cout << "Stack is Empty?" << stack.empty();
+}
+
+int main() 
+{
+    Stack stacktest;
+    fill_stack(stacktest,cin);
+    return 0;
 }
