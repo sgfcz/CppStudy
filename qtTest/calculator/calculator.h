fa0e3ebfe7ea4cb33ba24b568b91ac6b62545b11
sgfcz <973829105@qq.com>
2021-01-07 23:35:20 +0800

    add

---
+++ qtTest/calculator/calculator.h
@@ -0,0 +1,19 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QWidget>
+#include <Button
+
+class Calculator : public Qwidget
+{
+    Q_OBJECT
+
+public:
+    Calculator(QWidget *parent = nullptr);
+private slots:
+
+private:
+    Button
+}
+
+#endif
