5e7d443489d59bbf39850a62df1feec776af3e9f
sgfcz <973829105@.com>
2021-01-24 20:06:14 +0800

    up

--- DataStructuresAndAlgorithms/ch1OOP/private.cpp
+++ DataStructuresAndAlgorithms/ch1OOP/private.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -11,16 +12,23 @@ public:
     }
 
     void memberFunction1() {
-        cout << dataMeber1 << ' ' << dataMember2 << ' '
+        cout << dataMember1 << ' ' << dataMember2 << ' '
             << dataMember3 << endl;
     }
 
     void memberFunction2(int i, char *s = "unknown") {
-        dataMeber2 = i;
+        dataMember2 = i;
         cout << i << " received from " << s << endl;
     }
 protected:
-    char dataMeber1[20];
-    int dataMeber2; 
+    char dataMember1[20];
+    int dataMember2; 
     double dataMember3;
+};
+
+int main() 
+{
+    C object1("object1", 100, 200), object2("object"), object3;
+    object1.memberFunction2(12);
+    return 0;
 }
