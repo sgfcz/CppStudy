29e9e9fe206c33312ce3a75eb872e16cbc3b6aa6
sgfcz <973829105@qq.com>
2021-01-25 23:13:47 +0800

    up codeeditor

--- qtTest/codeEditor/codeeditor.cpp
+++ qtTest/codeEditor/codeeditor.cpp
@@ -16,7 +16,15 @@ CodeEditor::CodeEditor(QWidget *parent) : QPlainTextEdit(parent)
 
 int CodeEditor::lineNumberAreaWidth()
 {
+    int digits = 1;
+    int max = qMax(1, blockCount());
+    while (max >= 10) {
+        max /= 10;
+        ++digits;
+    }
+    int space = 3 + fontMetrics().horizontalAdvance(QLatin1Char('9')) * digits;
 
+    return space;
 }
 
 void CodeEditor::updateLineNumberAreaWidth(int /* newBlockCount */)
@@ -26,17 +34,30 @@ void CodeEditor::updateLineNumberAreaWidth(int /* newBlockCount */)
 
 void CodeEditor::updateLineNumberArea(const QRect &rect, int dy)
 {
-
+    if (dy)
+        lineNumberArea->scroll(0, dy);
+    else
+        lineNumberArea->update(0, rect.y(), lineNumberArea->width(), rect.height());
+    
+    if (rect.contains(viewport()->rect()))
+        updateLineNumberAreaWidth(0);
 }
 
 void CodeEditor::resizeEvent(QResizeEvent *e)
 {
+    QPlainTextEdit::resizeEvent(e);
 
+    QRect cr = contentsRect();
+    lineNumberArea->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
 }
 
 void CodeEditor::highlighCurrentLine()
 {
+    QList<QTextEdit::ExtraSelection> extraSelections;
 
+    if (!isReadOnly()) {
+        
+    }
 }
 
 void CodeEditor::lineNumberAreaPaintEvent(QPaintEvent *event)
