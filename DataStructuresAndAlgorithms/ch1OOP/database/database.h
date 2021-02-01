e7d0d11ffd5407fe94c82ef23941551b5c9670ba
sgfcz <973829105@qq.com>
2021-02-01 23:44:49 +0800

    add database

--- DataStructuresAndAlgorithms/ch1OOP/database/database.h
+++ DataStructuresAndAlgorithms/ch1OOP/database/database.h
@@ -10,14 +10,14 @@ class Database {
 public:
     Database();
     void run();
-protected:
+private:
     fstream database;
     char fName[20];
     ostream& print(ostream&);
     void add(T&);
     bool find(const T&);
     void modify(const T&);
-    friend ostream& operator<<(ostream out, Database& db) {
+    friend ostream& operator<<(ostream& out, Database& db) {
         return db.print(out);
     }
 };
