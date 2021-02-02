c8c254076ee17bf7520050e4f0c3c74c3790e742
sgfcz <973829105@qq.com>
2021-02-03 01:36:03 +0800

    Add Linked List

--- DataStructuresAndAlgorithms/ch3Linked_List/Singly_Linked_List/intSLList.h
+++ DataStructuresAndAlgorithms/ch3Linked_List/Singly_Linked_List/intSLList.h
@@ -15,4 +15,23 @@ public:
     IntSLLNode *next;
 };
 
+class IntSLList {
+public:
+    IntSLList() {
+        head = tail = 0;
+    }
+    ~IntSLList();
+    int isEmpty() {
+        return head == 0;
+    }
+    void addToHead(int);
+    void addToTail(int);
+    int deleteFromHead();
+    int deleteFromTail();
+    void deleteNode(int);
+    bool isInList(int) const;
+private:
+    IntSLLNode *head, *tail;
+};
+
 #endif
